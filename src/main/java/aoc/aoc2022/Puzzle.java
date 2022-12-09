65421e34caf2be2dd30128eea66bc3fb6d0bef0e
Paul Rapoport <paul.rapoport@experian.com>
2022-12-09 17:11:01 -0500

    update Puzzle

--- src/main/java/aoc/aoc2022/Puzzle.java
+++ src/main/java/aoc/aoc2022/Puzzle.java
@@ -1,23 +1,10 @@
 package aoc.aoc2022;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
-
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class Puzzle {
-    public static String SRC_MAIN_RESOURCES = "src/main/resources/aoc2022/";
-    public static String FILE_NAME = "day%d%s.txt";
-    RandomAccessFile inputFile;
-
-    static RandomAccessFile getFile(int day) throws Exception {
-
-        String a=  String.format(FILE_NAME, day, "-test");
-        try (RandomAccessFile file = new RandomAccessFile(SRC_MAIN_RESOURCES + String.format(FILE_NAME, day, "-test"), "r")) {
-            return file;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public static final String SRC_MAIN_RESOURCES = "src/main/resources/aoc2022/";
+    public static final String FILE_NAME = "day%d%s.txt";
 }
