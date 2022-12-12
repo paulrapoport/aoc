cf4de8581684fd2a7c52b8084ae1c543f4ec9957
Paul Rapoport <paul.rapoport@experian.com>
2022-12-12 14:13:09 -0500

    Update dependencies

--- src/main/java/aoc/aoc2022/Puzzle.java
+++ src/main/java/aoc/aoc2022/Puzzle.java
@@ -1,9 +1,8 @@
 package aoc.aoc2022;
 
-import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public abstract class Puzzle {
     public static final String SRC_MAIN_RESOURCES = "src/main/resources/aoc2022/";
     public static final String FILE_NAME = "day%d%s.txt";
